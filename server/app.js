60a22dce7190a2d2608d8a6321631673a1367e0d
mr-loop-1 <loopolecreator1@gmail.com>
2024-10-13 08:54:47 +0530

    add: folders

---
+++ server/app.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use("", (req, res) => {
+    res.status(200).json({ message: "Server is live" });
+});
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("listning...");
+});
